6ba93c3201682fe56fba0cf4e22720e0235c99ed
Brannon <57905614+BrannonOpperman@users.noreply.github.com>
2019-12-10 17:10:06 +0200

    Created ProjectDetails and ProjectSummary. Nested ProjectSummary into Dashboard.

---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const ProjectDetails = () => {
+    return (
+        <div className="container section project-details">
+            <div className="card z-depth-0">
+                <div className="card content">
+                    <span className="card-title">Project Title</span>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero qui ut officia alias nostrum aperiam assumenda repellendus omnis dolor! Dolorem excepturi a id quas. Maiores natus quia mollitia ullam magnam.</p>
+                </div>
+                <div className="card-action grey lighten-4 grey-text">
+                    <div>Posted by Brannon Opperman</div>
+                    <div>2nd September, 4am</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectDetails
